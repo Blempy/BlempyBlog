c4fc8204a85a8f2dd06465638adb82a91f5fe65d
P.Oliten <hamiccc@gmail.com>
2022-06-23 17:07:10 +0200

    First Commit - Django Blog

---
+++ src/posts/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from django.views.generic import ListView
+
+from posts.models import BlogPost
+
+
+class BlogHome(ListView):
+    model = BlogPost
+    context_object_name = "posts"
